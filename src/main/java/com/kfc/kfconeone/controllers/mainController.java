00ae2fcca507f78db7af0fc15e5393f9d6ee8a0e
z c <z c>
2017-10-21 12:01:34 +0800

    完成取得球場資訊

--- src/main/java/com/kfc/kfconeone/controllers/mainController.java
+++ src/main/java/com/kfc/kfconeone/controllers/mainController.java
@@ -2,7 +2,9 @@ package com.kfc.kfconeone.controllers;
 
 
 import com.kfc.kfconeone.models.PlayerInfo;
+import com.kfc.kfconeone.repository.CourtInfoRepository;
 import com.kfc.kfconeone.repository.PlayerInfoRepository;
+import com.kfc.kfconeone.repository.UpdateInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,20 +13,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.sql.Timestamp;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller    // 說明這個class是Controller
 public class mainController {
 
-    @Autowired // 這標籤代表會取得自動產生的操作元件UserRepository
+    @Autowired // 這標籤代表會取得自動產生的操作元件
     private PlayerInfoRepository playerInfoRepository;
+    @Autowired
+    private UpdateInfoRepository updateInfoRepository;
+    @Autowired
+    private CourtInfoRepository courtInfoRepository;
 
     @RequestMapping(path="/getaccount",method = RequestMethod.POST)
     public @ResponseBody
-    Map addUser (@RequestBody Map _req) {
+    Map getaccount (@RequestBody Map _req) {
 
         Map res = new HashMap();
         try
@@ -53,6 +61,44 @@ public class mainController {
         }
         catch (Exception ex)
         {
+            res.put("result","100 failed");
+        }
+
+        return res;
+    }
+
+    @RequestMapping(path="/getcourtsinfo",method = RequestMethod.POST)
+    public @ResponseBody
+    Map getCourtsinfo (@RequestBody Map _req) {
+
+        Map res = new HashMap();
+        try
+        {
+            System.out.println(_req.get("courtsLastUpdate"));
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            ZonedDateTime zdt =ZonedDateTime.parse(_req.get("courtsLastUpdate").toString(),formatter.withZone(ZoneId.systemDefault()));
+
+            Timestamp playerUpdateTime = Timestamp.from(zdt.toInstant());
+            Timestamp systemUpdateTime = updateInfoRepository.findAll().iterator().next().getCourtFlag();
+
+            if(systemUpdateTime.after(playerUpdateTime))
+            {
+                res.put("isNeedUpdate",true);
+                res.put("courtsInfo", courtInfoRepository.findAll());
+            }
+            else
+            {
+                res.put("isNeedUpdate",false);
+            }
+
+
+
+            res.put("result","000 sucess");
+
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
             res.put("result","100 failed");
         }
 
---
+++ src/main/java/com/kfc/kfconeone/repository/CourtInfoRepository.java
@@ -0,0 +1,7 @@
+package com.kfc.kfconeone.repository;
+
+import com.kfc.kfconeone.models.CourtInfo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourtInfoRepository extends CrudRepository<CourtInfo,Long> {
+}
